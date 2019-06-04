156c7a94bf4fee160554ddd933d7b28b39f60d05
Alex <45273710+AlexBardadym@users.noreply.github.com>
2019-06-04 15:09:50 +0300

    build

---
+++ precache-manifest.80b423bfacddc8a5064fadfb9491d7d2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a5ac83691dba45cd3429fe8303fd22",
+    "url": "/AlexBardadym/alexbardadym.github.io/index.html"
+  },
+  {
+    "revision": "e3da63c3ae784f5e7f2f",
+    "url": "/AlexBardadym/alexbardadym.github.io/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "75543facb6bd1a994fc8",
+    "url": "/AlexBardadym/alexbardadym.github.io/static/css/main.75ba1714.chunk.css"
+  },
+  {
+    "revision": "e3da63c3ae784f5e7f2f",
+    "url": "/AlexBardadym/alexbardadym.github.io/static/js/2.eabafe13.chunk.js"
+  },
+  {
+    "revision": "75543facb6bd1a994fc8",
+    "url": "/AlexBardadym/alexbardadym.github.io/static/js/main.f0aa030f.chunk.js"
+  },
+  {
+    "revision": "686f56cb13629864e5c3",
+    "url": "/AlexBardadym/alexbardadym.github.io/static/js/runtime~main.72e29c5b.js"
+  },
+  {
+    "revision": "209f1cd7709c32348c9687a680b688a5",
+    "url": "/AlexBardadym/alexbardadym.github.io/static/media/find1.209f1cd7.svg"
+  },
+  {
+    "revision": "82c6ed6bd5e2eacd5556e0bb7b3e42f1",
+    "url": "/AlexBardadym/alexbardadym.github.io/static/media/find2.82c6ed6b.svg"
+  },
+  {
+    "revision": "40e7f305ab8d0e89fa71b5649f81dc9a",
+    "url": "/AlexBardadym/alexbardadym.github.io/static/media/geoposition.40e7f305.svg"
+  }
+]);
